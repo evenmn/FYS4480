57669799a3fba1b6421852a5429bff8395755f24
evenmn <evenmn@fys.uio.no>
2018-10-15 18:48:50 +0200

    Added new matrix, still get wrong energies

--- doc/Project1/source/main.py
+++ doc/Project1/source/main.py
@@ -25,13 +25,35 @@ if __name__ == '__main__':
     u = TBME(Z=1)
     v = OBME(Z=1)
     
-    A = np.zeros((3,3))
-    
+    A = np.zeros((5,5))
     A[0,0] = ground_state(u, v)
-    for i in range(1,3):
-        for j in range(1,3):
-            A[i,j] = single_excitations(i, j, u, v)
+    
+    # <12|H|21>
+    A[1,1] = single_excitations(1,1,u,v)
+    A[1,2] = single_excitations(1,1,u,v)
+    A[2,1] = single_excitations(1,1,u,v)
+    A[2,2] = single_excitations(1,1,u,v)
+    
+    # <13|H|21>
+    A[1,3] = single_excitations(1,2,u,v)
+    A[1,4] = single_excitations(1,2,u,v)
+    A[2,3] = single_excitations(1,2,u,v)
+    A[2,4] = single_excitations(1,2,u,v)
+    
+    # <12|H|31>
+    A[3,1] = single_excitations(2,1,u,v)
+    A[3,2] = single_excitations(2,1,u,v)
+    A[4,1] = single_excitations(2,1,u,v)
+    A[4,2] = single_excitations(2,1,u,v)
+    
+    # <13|H|31>
+    A[3,3] = single_excitations(2,2,u,v)
+    A[3,4] = single_excitations(2,2,u,v)
+    A[4,3] = single_excitations(2,2,u,v)
+    A[4,4] = single_excitations(2,2,u,v)
+    
+    print(A)
     
     eigvals, eigvecs = np.linalg.eigh(A)
-    print(eigvals)
+    print(eigvecs)
     
