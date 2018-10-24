ba82dd7e4df2033976f3b0d25d0ea039d1b1a701
evenmn <evenmn@fys.uio.no>
2018-10-24 23:08:54 +0200

    Organized code in classes

--- doc/Project1/source/CIS.py
+++ doc/Project1/source/CIS.py
@@ -4,70 +4,48 @@ from Matrix_elements import *
 class CIS:
     '''Configuration Interction Singles class for atomic structure'''
     
-    def __init__(self, Z, S=2):
+    def __init__(self, Z, basis):
         '''
         Arguments:
         ----------
         Z:      Int.
                 Atomic number (proton number)
-        S:      Int.
-                Number of spin states, should be fixed to 2
         '''
         
-        self.S = S
-        self.v = OBME(Z)
-        self.Z = Z
-        self.Orbits = int(Z/2)          # Assume fully occupied GS orbits
+        self.Elements = Integrals(Z,basis)  # Matrix elements/integrals
+        self.n = Z                          # Assume neutral atom
 
 
     def c_H_c(self):
         '''Reference energy'''
         
-        v = self.v
-        S = self.S
-        Z = self.Z
-        Orbits = self.Orbits
-        
         OBT = 0
-        for i in range(Orbits):
-            for i_s in range(S):
-                OBT += v[i,i]
+        for i in range(self.n):
+            OBT += self.Elements.OBME(i,i)
                 
         TBT = 0
-        for i in range(Orbits):
-            for j in range(Orbits):
-                for i_s in range(S):
-                    for j_s in range(S):
-                        TBT += 0.5*s2r_antisym(Z,i,j,i,j, i_s,j_s,i_s,j_s)
+        for i in range(self.n):
+            for j in range(self.n):
+                TBT += 0.5*self.Elements.Antisym(i,j,i,j)
         
         return OBT + TBT
         
         
-    def c_H_ia(self, i,a,i_s,a_s):
+    def c_H_ia(self, i,a):
         '''hggh'''
         
-        v = self.v
-        S = self.S
-        Z = self.Z
-        Orbits = self.Orbits
-        
-        OBT = v[i,a]
+        OBT = self.Elements.OBME(i,a)
         
         TBT = 0
-        for j in range(Orbits):
-            for j_s in range(S):
-                TBT += s2r_antisym(Z,a,j,i,j, a_s,j_s,i_s,j_s)
+        for j in range(self.n):
+            TBT += self.Elements.Antisym(a,j,i,j)
         
         return OBT + TBT
         
         
-    def ia_H_jb(self, i,a,j,b,i_s,a_s,j_s,b_s):
+    def ia_H_jb(self, i,a,j,b):
         '''hhh'''
         
-        v = self.v
-        S = self.S
-        Z = self.Z
-        Orbits = self.Orbits
         
         '''
         Result = s2r_antisym(Z,a,j,i,b, a_s,j_s,i_s,b_s)
@@ -78,27 +56,31 @@ class CIS:
             Result += v[i,j]
         
         '''
-        Result = s2r(Z,a,j,i,b, a_s,j_s,i_s,b_s)
+        Result = self.Elements.TBME(a,j,i,b)
         
-        if a==b and a_s==b_s:
-            Result -= v[i,j]
-            for k in range(Orbits):
-                for k_s in range(S):
-                    Result -= s2r_antisym(Z,i,k,j,k, i_s,k_s,j_s,k_s)
+        if a==b:
+            Result -= self.Elements.OBME(i,j)
+            for k in range(self.n):
+                Result -= self.Elements.Antisym(i,k,j,k)
                     
-            if i==j and i_s==j_s:
-                for k in range(Orbits):
-                    for k_s in range(S):
-                        Result += v[k,k]
-                        for l in range(Orbits):
-                            for l_s in range(S):
-                                Result += 0.5*s2r_antisym(Z,k,l,k,l, k_s,l_s,k_s,l_s)
+            if i==j:
+                for k in range(self.n):
+                    Result += self.Elements.OBME(k,k)
+                    for l in range(self.n):
+                        Result += 0.5*self.Elements.Antisym(k,l,k,l)
                                 
-        if i==j and i_s==j_s:
-            Result += v[a,b]
-            for k in range(Orbits):
-                for k_s in range(S):
-                    Result += s2r_antisym(Z,a,k,b,k, a_s,k_s,b_s,k_s)
+        if i==j:
+            Result += self.Elements.OBME(a,b)
+            for k in range(self.n):
+                Result += self.Elements.Antisym(a,k,b,k)
         
         
         return Result
+        
+if __name__ == '__main__':
+    basis = ((0,0), (0,1), (1,0), (1,1), (2,0), (2,1))
+    
+    Helium = CIS(2,basis)
+    
+    print(Helium.c_H_c())
+    print(Helium.c_H_ia(0,4))
--- doc/Project1/source/Hartree_Fock.py
+++ doc/Project1/source/Hartree_Fock.py
@@ -18,18 +18,17 @@ def h_HF_elements(i,j, C, Z, S=2):
         j = j-3
         j_s = 1
     
-    C_conj = np.conj(C)
     v = OBME(Z)
     
     Result = v[i, j]
     
-    for p in range(3):
-        for p_s in range(S):
-            for c in range(3):
-                for c_s in range(S):
-                    for d in range(3):
-                        for d_s in range(S):
-                            Result += C_conj[S*p+p_s,S*c+c_s]*C[S*p+p_s,S*d+d_s]*s2r_antisym(Z, i,c,j,d, i_s,c_s,j_s,d_s)
+    for p in range(2):
+      for p_s in range(S):
+        for c in range(2):
+          for c_s in range(S):
+            for d in range(2):
+              for d_s in range(S):
+                Result += C[S*p+p_s,S*c+c_s]*C[S*p+p_s,S*d+d_s]*s2r_antisym(Z, i,c,j,d, i_s,c_s,j_s,d_s)
                             
     return Result
     
@@ -54,7 +53,8 @@ def h_HF_iter(Z):
     
     print(eigvals)
     
-    while abs(eigvals[0]) > 0.001:
+    #while abs(eigvals[0]) > 0.001:
+    for i in range(10):
         HF = h_HF_matrix(C, Z)
         eigvals, C = np.linalg.eigh(HF)
         print(eigvals)
@@ -62,6 +62,29 @@ def h_HF_iter(Z):
     
     return C
     
+    
+def calc_E(C, Z, S=2):
+    '''Calculate energy'''
+    
+    v = OBME(Z)
+    
+    E = 0
+    for p in range(2):
+      for a in range(3):
+        for a_s in range(S):
+          for b in range(3):
+            for b_s in range(S):
+              E += C[p,a]*C[p,b]*v[a,b]
+              for q in range(2):
+                for c in range(3):
+                  for c_s in range(S):
+                    for d in range(3):
+                      for d_s in range(S):
+                        E += C[p,S*a+a_s]*C[q,S*b+b_s]*C[p,S*c+c_s]*C[q,S*d+d_s]*s2r_antisym(Z, a,b,c,d, a_s,b_s,c_s,d_s)
+                        
+    return E
+    
 
 if __name__ == '__main__':
-    h_HF_iter(2)
+    C = h_HF_iter(2)
+    print(calc_E(C, Z=2))
